13d5823e1b9c2c3ed8f86f09ad9016719b373816
Gabriel Hołubowicz <brysiekholub@gmail.com>
2024-01-23 16:18:17 +0100

    feat(backend):add job offer routes

--- src/routes/candidate.ts
+++ src/routes/candidate.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { sendCandidate, uploadPdf } from "../controllers/candidate";
+import {
+  getCandidates,
+  getSingleCandidates,
+  sendCandidate,
+  uploadPdf,
+} from "../controllers/candidate";
 import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
 router.post("/send", uploadPdf, sendCandidate);
-router.get("/", verifyToken);
-router.get("/:id");
+router.get("/", verifyToken, getCandidates);
+router.get("/:id", verifyToken, getSingleCandidates);
--- src/routes/jobOffer.ts
+++ src/routes/jobOffer.ts
@@ -2,12 +2,24 @@ import express, { Request, Response } from "express";
 import verifyToken from "../middleware/auth";
 import { JobOfferType } from "../models/type";
 import JobOffer from "../models/jobOffer";
-import { createOffer, getOffers } from "../controllers/jobOffer";
+import {
+  createOffer,
+  deleteOffeer,
+  getOffers,
+  getSingleOffer,
+  updateOffer,
+} from "../controllers/jobOffer";
 
 const router = express.Router();
 
 router.post("/", verifyToken, createOffer);
 
 router.get("/", getOffers);
+
+router.get("/id", getSingleOffer);
+
+router.put("/id", verifyToken, updateOffer);
+
+router.delete("/id", verifyToken, deleteOffeer);
 
 export default router;
