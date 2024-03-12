8a7c04afec5ba7527dec947cec8a5f15f492af2a
Gabriel Hołubowicz <gabriel.holubowicz@b4limits.pl>
2024-03-12 10:39:33 +0100

    feat: change api to offer page

--- src/controllers/jobOffer.ts
+++ src/controllers/jobOffer.ts
@@ -49,7 +49,15 @@ export const getOffers = async (req: Request, res: Response) => {
       body.workDirection = filterOffer.workDirection;
     }
     const jobOffers = await JobOffer.find(body);
-    res.json(jobOffers);
+    const companyOffer = await Company.find().select("-email -password");
+
+    const allOffers = jobOffers.map((offer) => {
+      return {
+        offer,
+        company: companyOffer.find((it) => it._id == offer.companyId),
+      };
+    });
+    res.json(allOffers);
   } catch (e) {
     res.status(500).json({ message: "Error, no job offers" });
   }
@@ -81,20 +89,25 @@ export const getCompanyOffersInfo = async (req: Request, res: Response) => {
 export const getSingleOffer = async (req: Request, res: Response) => {
   try {
     const offer = await JobOffer.findOne({ _id: req.params.offerId });
-    res.json(offer);
+
+    const company = await Company.findById(offer?.companyId).select(
+      "-email -password"
+    );
+
+    res.json({ offer, company });
   } catch (e) {
-    res.status(500).json({ message: "Something wrong" });
+    res.status(500).json({ message: e });
   }
 };
 
 export const getRecomendedOffer = async (req: Request, res: Response) => {
   try {
     const count = await JobOffer.countDocuments({});
-    const random = Math.floor(Math.random() * count);
+    const random = Math.floor(Math.random() * (count - 3));
     const offer = await JobOffer.find().skip(random).limit(3);
     res.json(offer);
   } catch (e) {
-    res.status(500).json({ message: "bleble" });
+    res.status(500).json({ message: e });
   }
 };
 
